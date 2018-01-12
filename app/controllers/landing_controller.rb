3987a2c5a1d3db473e1556ade8b090acbe07f579
oliver_ochman <oliver.ochman@hotmail.com>
2018-01-12 16:03:05 +0100

    removed/changed bad code

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,11 +2,4 @@ class LandingController < ApplicationController
   def index
     @articles = Article.all
   end
-
-  private
-
-  def article_timestamp
-    time = params[:article].permit(:created_at)
-    time.strftime('at %F')
-  end
 end
